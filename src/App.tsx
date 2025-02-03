83fa9387c06cfd64084b4f11bce29f8363323e4b
Francisco Sánchez <sanchezgarridof@gmail.com>
2025-02-03 16:00:12 +0100

    add: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import './App.css'
 
 import UsersList from './components/UsersList'
 import UserProfile from './components/UserProfile';
+import { ThemeProvider } from "./context/theme-provider";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path='/' element={<UsersList/>} />
-          <Route path='/users/:userId' element={<UserProfile/>} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path='/' element={<UsersList/>} />
+            <Route path='/users/:userId' element={<UserProfile/>} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   )
 }
 
